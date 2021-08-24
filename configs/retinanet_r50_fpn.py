b7563afdccc9f645483fc8b8c209bdf0f688d97d
mmeendez8 <miguelmndez@gmail.com>
2021-08-24 11:55:35 +0200

    Fix num_classes in model config

--- configs/retinanet_r50_fpn.py
+++ configs/retinanet_r50_fpn.py
@@ -22,7 +22,7 @@ model = dict(
     ),
     bbox_head=dict(
         type="RetinaHead",
-        num_classes=80,
+        num_classes=6,
         in_channels=256,
         stacked_convs=4,
         feat_channels=256,
