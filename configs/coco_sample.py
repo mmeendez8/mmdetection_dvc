4929c478cae2b18f9a31fb448b08d3686b9f7348
mmeendez8 <miguelmndez@gmail.com>
2021-08-25 10:48:23 +0200

    Reduce `img_scale`

--- configs/coco_sample.py
+++ configs/coco_sample.py
@@ -5,6 +5,7 @@ img_folder = "data/coco_sample/train_sample"
 
 CLASSES = ["chair", "couch", "tv", "remote", "book", "vase"]
 
+img_scale = (512, 512)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True
 )
@@ -12,7 +13,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(type="LoadAnnotations", with_bbox=True),
-    dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
+    dict(type="Resize", img_scale=img_scale, keep_ratio=True),
     dict(type="RandomFlip", flip_ratio=0.5),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="Pad", size_divisor=32),
@@ -23,7 +24,7 @@ test_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(
         type="MultiScaleFlipAug",
-        img_scale=(1333, 800),
+        img_scale=img_scale,
         flip=False,
         transforms=[
             dict(type="Resize", keep_ratio=True),
